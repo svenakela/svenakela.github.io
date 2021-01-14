825d0950001b56a1f02709bcd8371b96f0ebfe85
Sven Wesley <svenne.duva@gmail.com>
2021-01-14 20:34:52 +0100

    Scroll auto added when attribute scrollTo exists, no comment

--- flyer/effect.js
+++ flyer/effect.js
@@ -6,5 +6,4 @@ document.querySelectorAll('[scrollTo]').forEach(element => {
     var insertThis = '<div class="scrolldown" onclick="goToAnchor(\'xxx\')">Scroll down<br>&#8675;</div>'
         .replace('xxx', element.getAttribute('scrollTo'));
     element.insertAdjacentHTML('beforeend', insertThis);
-    // element.addEventListener('click', () => { goToAnchor(element.getAttribute('scrollTo')) });
 });
