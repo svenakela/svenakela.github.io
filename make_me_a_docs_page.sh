83103b81d076b189274344110b87e96b0a6da2a4
Sven Wesley <sven.wesley@alpcot.se>
2025-02-19 19:46:59 +0100

    Reworking it

--- make_me_a_docs_page.sh
+++ make_me_a_docs_page.sh
@@ -2,4 +2,4 @@
 
 rm -rf docs/*
 
-mvn clean compile site exec:java -Dexec.mainClass=tech.docs.SearchIndexer
+mvn clean compile generate-resources site exec:java -Dexec.mainClass=tech.docs.SearchIndexer
